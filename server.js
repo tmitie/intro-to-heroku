61c57f3daf35af2874ede7471abba541b8ffb44f
Thaynara Tessaline Mitie Sei Soares <thaynarats@weg.net>
2021-03-25 11:40:42 -0300

    query changed

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/broker/:sfid', function (req, res) {
 });
 
 app.get('/alimentador', function (req, res) {
-  client.query('SELECT comando_para_placa__c FROM ' + alimentadorTable, function (error, data) {
+  client.query('SELECT comando_para_placa__c, id FROM ' + alimentadorTable, function (error, data) {
     res.json(data.rows);
     console.log("Alimentador 0000: " + data.rows[0]);
     console.log("Alimentador: " + data.rows);
